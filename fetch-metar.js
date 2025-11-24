c35b0d2b45f8ffdfcc4215ae76b8a74f87204ea5
Slushie <57730247+slushie0@users.noreply.github.com>
2025-11-24 14:54:38 -0800

    Refactor fetch-metar.js to remove unused code
    Removed CSV parsing and search functionality for airport data.

--- fetch-metar.js
+++ fetch-metar.js
@@ -1,15 +1,5 @@
-let data = {};
-Papa.parse("airport-codes.csv", {
-  download: true,
-  header: true,
-  complete: function (results) {
-    data = results.data.map(element => ({ ident: element.ident, type: element.type, name: element.name, municipality: element.municipality }))
-    //console.log("Finished:", data);
-  }
-});
-
 window.addEventListener('click', function (e) {
-  let div = document.querySelector("#search-results");
+  let div = document.querySelector("#search-block");
   if (document.getElementById('query').contains(e.target)) {
     div.classList.remove("visually-hidden");
   } else {
@@ -35,36 +25,6 @@ let cloudDesc = {
   SKC: "Sky Clear",
   VV: "Vertical Visibility"
 };
-
-function search() {
-  let query = document.querySelector("#query");
-  let searchResults = document.querySelector("#search-results");
-  searchResults.innerHTML = "";
-  if (query.value.length < 1) {
-    return;
-  }
-  let regex = new RegExp(query.value, 'i');
-  let newdata = data.filter((item) => Object.values(item).find((value) => regex.test(value)));
-  newdata.sort((a, b) => sizeOrder[a.type] - sizeOrder[b.type]);
-
-  //newdata = newdata.slice(0, 20);
-  for (let i = 0; i < 20; i++) {
-    if (newdata.length <= i) { return; }
-    console.log(newdata[i].type);
-    let ident = newdata[i].ident;
-    let name = newdata[i].name;
-    let municipality = newdata[i].municipality;
-    let item = document.createElement("a");
-    item.innerHTML = '<span>' + ident + '</span> - <span>' + name + '</span> - <span>' + municipality + '</span>';
-    item.className = "list-group-item list-group-item-action";
-    item.href = "#";
-    item.onclick = () => {
-      query.value = ident;
-      getMetar();
-    }
-    searchResults.appendChild(item);
-  }
-}
 
 let latestMetars = [];
 let latestAirports = []
